fca32770ab9a2297128a0d0b7d85e8433399f7aa
Nico Piel <nico.piel@hotmail.de>
2025-10-17 17:23:35 +0200

    Prettier config and proxy

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,21 +5,21 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 import Header from '../components/Header'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Header />
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
-  ),
+    component: () => (
+        <>
+            <Header />
+            <Outlet />
+            <TanStackDevtools
+                config={{
+                    position: 'bottom-right',
+                }}
+                plugins={[
+                    {
+                        name: 'Tanstack Router',
+                        render: <TanStackRouterDevtoolsPanel />,
+                    },
+                ]}
+            />
+        </>
+    ),
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [
+        TanStackRouterVite({ autoCodeSplitting: true }),
+        viteReact(),
+        tailwindcss(),
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
     },
-  },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://localhost:8443',
+                changeOrigin: true,
+            },
+        },
+    },
 })
