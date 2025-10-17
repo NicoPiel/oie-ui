efc46a93f482286a3f6a476efea988e9fb3a79c6
Nico Piel <nico.piel@hotmail.de>
2025-10-17 17:15:55 +0200

    Move away from BFF to SPA

--- src/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { usernameClient } from 'better-auth/client/plugins';
-import { createAuthClient } from 'better-auth/react';
-
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: import.meta.env.VITE_BETTER_AUTH_URL,
-    plugins: [usernameClient()],
-});
